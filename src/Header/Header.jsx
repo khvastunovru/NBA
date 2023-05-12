34d0bee0e9aba0ed4e693e644e033ee107db1c66
khvastunovru <khvastunov.ru@mail.ru>
2023-05-12 15:55:38 +0300

    загрузка nba

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <ul className="nav navbar bg-dark" data-bs-theme="dark">
-      <li><Link to="/" >Главная</Link>   </li>
+      <li><Link to="/" >Главная страница</Link>   </li>
       <li><Link to="/players">Игроки</Link> </li>
       <li><Link to="/teams">Команды</Link> </li>
       <li><Link to="/games">Игры</Link> </li>
